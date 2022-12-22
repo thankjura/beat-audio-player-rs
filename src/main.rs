21c595d8c0c967acfdc2ab07bf2290102cf599e5
thankjura <me@slie.ru>
2022-12-23 00:15:30 +0300

    bump

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod structs;
 mod utils;
 mod config;
 
-use config::{GETTEXT_PACKAGE, LOCALEDIR, PKGDATADIR};
+use config::{GETTEXT_PACKAGE, LOCALEDIR};
 use gettextrs::*;
 use gtk::prelude::*;
 use gtk::gio;
@@ -16,16 +16,29 @@ use crate::app::BeatApp;
 
 const APP_ID: &str = "ru.slie.beat";
 
+#[cfg(not(debug_assertions))]
+fn load_resources() {
+    use config::LOCALEDIR;
+    let resources = gio::Resource::load(PKGDATADIR.to_owned() + "/beat.gresource")
+        .expect("Could not load resources");
+    gio::resources_register(&resources);
+}
+
+#[cfg(debug_assertions)]
+fn load_resources() {
+    gio::resources_register_include!("beat.gresource")
+        .expect(&gettext("Could not load resources"));
+}
 
 fn main() {
+    setlocale(LocaleCategory::LcAll, "");
     bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR).expect("Unable to bind the text domain");
     bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8")
         .expect("Unable to set the text domain encoding");
     textdomain(GETTEXT_PACKAGE).expect("Unable to switch to the text domain");
 
-    let resources = gio::Resource::load(PKGDATADIR.to_owned() + "/beat.gresource")
-        .expect("Could not load resources");
-    gio::resources_register(&resources);
+
+    load_resources();
 
     //gtk::init().unwrap();
     let app = BeatApp::new(APP_ID);
